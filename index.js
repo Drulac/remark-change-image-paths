cf30fda2d43b51cc841c352c7d4ce7038a6ce04b
drulac <drulac@protonmail.com>
2020-09-06 14:32:42 +0200

    remove abusive console.log

--- index.js
+++ index.js
@@ -7,27 +7,27 @@ module.exports = (
 	}
 ) => {
 	return function image() {
-		console.log(options)
+		//console.log(options)
 
 		return function transformer(tree) {
 			visit(tree, 'element', onImage)
 
 			function onImage(node) {
 				if (node.tagName === 'img') {
-					console.log(
+					/*console.log(
 						'replace  :',
 						JSON.stringify(node.properties.src)
-					)
+					)*/
 
 					node.properties.src = node.properties.src.replace(
 						options.search,
 						options.replace
 					)
 
-					console.log(
+					/*	console.log(
 						'replaced :',
 						JSON.stringify(node.properties.src)
-					)
+					)*/
 				}
 			}
 		}
